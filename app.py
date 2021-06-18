8c21ac6300d18576f8faf6d6db703af585e594b1
allengu01 <allen.gu01@gmail.com>
2021-06-17 21:35:46 -0500

    Wrote first route and received an image from app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "ClassifAI"
+
+@app.route("/api/v1.0/classify", methods=['POST'])
+def classify():
+    image = request.files['file']
+    image.save(os.path.join("./uploads", secure_filename(image.filename)))
+    return jsonify({"response": "HI"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ routes.md
@@ -0,0 +1,3 @@
+## Routes
+- POST - /api/v1.0/classify </br>
+    Takes in the image file and returns the classification data from TensorFlow
